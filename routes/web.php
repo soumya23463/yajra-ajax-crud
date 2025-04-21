3a55b99c683a9c5627297bb94230d0ffea8860a2
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-04-21 22:12:14 +0530

    crud done

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
+
 });
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
